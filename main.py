a7d912d33f54618c7525ee30139e4d1c6bf39872
jav1st <burzum268@yandex.ru>
2021-08-28 00:57:53 +0300

    stable ver, zero balance not wrk

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ def game():
             break
         if answ == 'balance':
             print(balance.read_balance())
-            break
+            continue
+
 
         result_player1 = throw()
         result_jav1st1 = throw()
@@ -37,14 +38,16 @@ def game():
         if result_jav1st1 == result_jav1st2:
             score_jav1st = score_jav1st * 2
             double_jav1st = True
-            double_jav1st = (f'\nКомпьютер выкинул дубль! Сумма выиграша умножена вдвое -  {score_jav1st}')
+            double_jav1st = f'\nКомпьютер выкинул дубль! Сумма выиграша умножена вдвое -  {score_jav1st}'
+
         elif result_player1 == result_player2:
             score_player = score_player * 2
             double_player = True
-            double_player = (f'\nПоздравляю, у Вас дубль! Сумма выиграша умножена вдвое - {score_player}')
+            double_player = f'\nПоздравляю, у Вас дубль! Сумма выиграша умножена вдвое - {score_player}'
 
         answ = int(answ)
         print(f'Ваша ставка {answ} монет')
+
         if answ <= int(balance.read_balance()):
             print(f'Вы выкинули', result_player1, 'и', result_player2, double_player)
             print(f'Компьютер кинул', result_jav1st1, 'и', result_jav1st2, double_jav1st)
@@ -54,27 +57,36 @@ def game():
                 print(f'Вам зачислоно {answ} монет.\nВаш баланс {money} монет')
                 balance.write_balance(money)
                 balance.read_balance()
+                continue
+
             elif score_jav1st == score_player:
                 print('Ничья!')
+                continue
+
             else:
                 print("Вы проиграли.")
                 money = balance.read_balance() - answ
                 print(f'Списано {answ} монет.\nВаш баланс {money} монет')
                 balance.write_balance(money)
-        if int(balance.read_balance()) == 0:
+                continue
+
+        if str(balance.read_balance()) == 0:
             answ = ('Вы проиграли все деньги! Начать новую игру?.\n Да\Нет').lower()
             if answ == 'да':
                 balance.zero()
                 game()
             if answ == 'нет':
                 break
+
         if answ >= int(balance.read_balance()):
             print(f'Ваш баланс {balance.read_balance()}'
                   f'\nУменьшите ставку')
             continue
+
         else:
             print('Неизвестная команда ')
             break
+
 
 
 def start():
